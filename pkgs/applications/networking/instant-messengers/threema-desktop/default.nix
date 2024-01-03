b4d80f1ff85034ed167a8bac6e3fee092db445a3
greenpsi <git@psinet.dev>
2024-01-03 17:15:59 +0100

    threema-desktop: 1.2.31→ 1.2.39

--- pkgs/applications/networking/instant-messengers/threema-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/threema-desktop/default.nix
@@ -3,12 +3,12 @@
 
 stdenv.mkDerivation rec {
   pname = "threema-desktop";
-  version = "1.2.31";
+  version = "1.2.39";
 
   src = fetchurl {
     # As Threema only offers a Latest Release url, the plan is to upload each
     # new release url to web.archive.org until their Github releases page gets populated.
-    url = "https://web.archive.org/web/20230731230034if_/https://releases.threema.ch/web-electron/v1/release/Threema-Latest.deb";
+    url = "https://web.archive.org/web/20231228192429/https://releases.threema.ch/web-electron/v1/release/Threema-Latest.deb";
     hash = "sha256-eZ/bjcSnrnzub1G4sbwPn3GCTwhDfFuYv9Plf5SJL90=";
   };
 
